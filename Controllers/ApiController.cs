eb012aac5a62a2629fd077eed5a12bb2fec5d7fc
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-12 18:19:09 -0600

    [1.10] add method - http delete

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -35,5 +35,17 @@ namespace WordApi.Controllers
             await _dataContext.SaveChangesAsync();
             return wordColor;
         }
+
+                // http delete member from collection
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id){
+            WordColor wordColor = await _dataContext.WordColors.FindAsync(id);
+            if (wordColor == null){
+                return NotFound();
+            }
+            _dataContext.Remove(wordColor);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
